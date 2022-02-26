afe0ccf927b12cf885beba2481683543e7982190
Ashu0126 <ashish.jaryal14@gmail.com>
2022-02-26 16:53:30 +0530

    intial commit

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./views");
+
 //middleware
-app.use(express.static("public"));
+app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 app.use(cookieParser());
 
 //app engine
+app.set("views", viewsPath);
 app.set("view engine", "ejs");
 
 //database connection
